0e460676fcd143396cd0e9117d21d54511d6c914
Juan Reyes <seyerman@gmail.com>
2019-02-20 18:59:05 -0500

    Greeting Java Project created using a JavaFX GUI.

---
+++ src/model/Greeting.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Greeting {
+	private final static String MORNING = "Morning"; 
+	private final static String AFTERNOON = "Afternoon"; 
+	private final static String NIGHT = "Night";
+	
+	
+}
---
+++ src/ui/GreetingController.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class GreetingController {
+
+    @FXML
+    private ComboBox<?> cbDayMoment;
+
+    @FXML
+    void greet(ActionEvent event) {
+    	
+    }
+
+}
